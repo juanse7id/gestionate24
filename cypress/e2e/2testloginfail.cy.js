a4eabac3e8025a12109ba2239a2038f58a67c764
juanse7id <juanse.7id@gmail.com>
2024-10-15 16:09:18 -0300

    2 test login fail

--- cypress/e2e/2testloginfail.cy.js
+++ cypress/e2e/2testloginfail.cy.js
@@ -1,11 +1,9 @@
-//<reference types="Cypress" /> 
 import selectores from './selectores/selectores';
 describe('login test fail', () => {
 var datosusuarios;
     beforeEach('pass test', () => {
-        //la url se encuentra en cypress.config.js
         cy.visit('/'); 
-        cy.wait(5000)
+        cy.wait(2000)
         cy.fixture('./users_fail')
         .then(login_test_fail => {
             datosusuarios = login_test_fail;
@@ -14,17 +12,16 @@ var datosusuarios;
 
 it('gestionate',()=>{
     datosusuarios.forEach((data) =>{
-        cy.get(selectores.login_select.users).click().type(data.Usuario1)
-        cy.get(selectores.login_select.password).click().type(data.Password1)
+        cy.get(selectores.login_select.users).click().type(data.Usuario)
+        cy.get(selectores.login_select.password).click().type(data.Password)
         cy.get(selectores.login_select.btn_iniciar_sesion).click()
-        cy.get('#mat-mdc-error-2').should('have.text', ' El usuario o la contraseña son incorrectos')
-        cy.wait(5000)
-        cy.get(selectores.login_select.users).click().clear().type(data.Usuario2)
-        cy.get(selectores.login_select.password).click().clear().type(data.Password2)
-
+        cy.get(selectores.login_select.msg_invald).should('be.visible')
+        cy.wait(1000)
+        cy.get(selectores.login_select.btn_iniciar_sesion).click()
+        cy.get(selectores.login_select.pop_up_verif_pass).should('be.visible')
+        cy.wait(2000)
+        cy.get(selectores.login_select.btn_cancelar).click()
     });
     });
 
 });
-
-//modificar users_fAIL.JSON
